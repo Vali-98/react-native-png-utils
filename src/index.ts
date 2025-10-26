dd1c76530b73744ed340bd045a9cde2bf4099937
Vali98 <al244098@gmail.com>
2025-10-26 19:20:46 +0800

    fix: docs

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { NitroModules } from 'react-native-nitro-modules'
 import type { PngUtils as PngUtilsSpec } from './specs/png-utils.nitro'
 
 const PngUtils = NitroModules.createHybridObject<PngUtilsSpec>('PngUtils')
+
 /**
  *
  * @param pngData base64 encoded PNG data
@@ -11,6 +12,12 @@ export function extractPngTextChunk(pngData: string) {
   return PngUtils.extractPngChunk(pngData)
 }
 
+/**
+ *
+ * @param fileData base64 encoded PNG data
+ * @param newData new tEXt chunk data
+ * @returns base64 encoded PNG with tEXt chunk replaced
+ */
 export function replacePngTextChunk(fileData: string, newData: string) {
   return PngUtils.replacePngChunk(fileData, newData)
 }
